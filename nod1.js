ce22fa80bf737581095fe6c59437480ebeb40a55
j <77177771+jumping283@users.noreply.github.com>
2022-12-03 11:40:57 -0800

    Create nod1.js

---
+++ nod1.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 69420;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('<img src="https://static.wikia.nocookie.net/roblox/images/1/1d/Roblox-Gametest1-2016.png/revision/latest/scale-to-width-down/1000?cb=20160206194450"><br><br><h1>carly its real test site!!');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
